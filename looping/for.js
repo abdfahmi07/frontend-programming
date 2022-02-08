7774955dac256823632bf3c2aebf0a90318e8060
Aufa Billah <aufaroot18@gmail.com>
2022-02-08 14:02:57 +0700

    Create looping

---
+++ looping/for.js
@@ -0,0 +1,7 @@
+/**
+ * Looping menggunakan for.
+ * Menampilkan angka 1 - 10.
+ */
+for (let i = 1; 1 < 11; i++) {
+  console.log(`Perulangan ke: ${i}`);
+}
---
+++ looping/while.js
@@ -0,0 +1,12 @@
+// membuat variable awal
+let i = 1;
+
+/**
+ * Looping menggunakan while.
+ * Membuat kondisi untuk batasan looping.
+ */
+while (i < 11) {
+  console.log(`Perulangan ke: ${i}`);
+  // melakukan increment
+  i++;
+}
