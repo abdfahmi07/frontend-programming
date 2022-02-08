1e0d5422d5f29755c158307de998c4975cb70640
Aufa Billah <aufaroot18@gmail.com>
2022-02-08 14:01:36 +0700

    create variable

---
+++ variable.js
@@ -0,0 +1,8 @@
+/**
+ * Membuat variable dengan keyword const.
+ * Variable const tidak dapat diubah nilainya.
+ */
+const name = "Aufa Billah";
+const major = "Informatics";
+
+console.log(name, major);
