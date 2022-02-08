97e4b9bee08e853923f57816c5da3af4fa694384
Aufa Billah <aufaroot18@gmail.com>
2022-02-08 14:02:48 +0700

    create conditional

---
+++ conditional/conditional.js
@@ -0,0 +1,16 @@
+const results = 85;
+
+// if: membuat satu kondisi
+if (results > 90) {
+  console.log("Grade: A");
+}
+// else if: membuat 2 kondisi atau lebih
+else if (results > 80) {
+  console.log("Grade: B");
+} else if (results > 70) {
+  console.log("Grade: C");
+}
+// else: membuat kondisi terakhir
+else {
+  console.log("Grade: D");
+}
---
+++ conditional/ternary-operator.js
@@ -0,0 +1,12 @@
+const age = 23;
+
+/**
+ * Ternary operator digunakan untuk membuat if else lebih singkat.
+ */
+age > 21 ? console.log("Dewasa") : console.log("Belum Dewasa");
+
+/**
+ * Ternary operator dapat digunakan untuk menyimpan nilai ke variable.
+ */
+const status = age > 21 ? "Dewasa" : "Belum Dewasa";
+console.log(status);
