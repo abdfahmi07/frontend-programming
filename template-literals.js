d5043dfcf325f546b0800a1693dc442b61ba2e7a
Aufa Billah <aufaroot18@gmail.com>
2022-02-08 14:02:10 +0700

    create template-literals

---
+++ template-literals.js
@@ -0,0 +1,14 @@
+const name = "Aufa Billah";
+const bod = 2001;
+
+/**
+ * Membuat string menggunakan template literals.
+ * Dapat menggunakan multiline.
+ * Dapat melakukan interpolasi.
+ */
+const greeting = `
+Hello, my name ${name}.
+Umur saya ${2022 - bod}
+`;
+
+console.log(greeting);
