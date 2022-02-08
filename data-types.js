4d31bd6883cb92b395449b9bf0aec65cc47b0778
Aufa Billah <aufaroot18@gmail.com>
2022-02-08 14:01:46 +0700

    create data-types

---
+++ data-types.js
@@ -0,0 +1,9 @@
+const name = "Aufa Billah"; // string
+const age = 23; // number
+const isMarried = false; // boolean
+let dateAt; // undefined
+
+/**
+ * operator typeof untuk mengecek tipe data.
+ */
+console.log(typeof name, typeof age);
