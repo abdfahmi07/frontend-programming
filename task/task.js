2114dbec47107305a8378452d5e7987f82cf85c1
Aufa Billah <aufaroot18@gmail.com>
2022-02-22 14:39:21 +0700

    Update task

--- task/task.js
+++ task/task.js
@@ -1,64 +1,58 @@
 /**
- * DESCRIPTION.
- * Tugas ini untuk mengelola struktur data array.
- * Ubah sintaks JavaScript ke JavaScript Modern.
- */
-
-/**
  * TODO 1.
- * Create array of object users (5 users).
- * Object has property: name, age, major.
- * Note: Use const instead var.
+ * Buat array of object users (5 users).
+ * Object memiliki property: name, age, major.
+ * Note: Ubah var menggunakan JavaScript Modern.
  */
 var users;
 
 /**
  * TODO 2
- * Create all function: Show all data users.
- * Hint: use for/for-of.
- * Note: Use arrow function and const.
+ * Buat function all: Menampilkan semua data user.
+ * Hint: Gunakan for/for-of.
+ * Note: Ubah function menggunakan arrow function.
  */
 function all() {}
 
 /**
  * TODO 3
- * Create store function: Add data to users.
- * Hint: use push method.
- * Note: Use arrow function and const.
+ * Buat function store: Menambahkan user baru.
+ * Hint: Gunakan method push.
+ * Note: Ubah function menggunakan arrow function.
  */
 function store(user) {}
 
 /**
  * TODO 4.
- * Create update function: Edit data user.
- * Hint: just re-assign.
- * Note: Use arrow function and const.
+ * Buat function update: Mengedit data user.
+ * Hint: re-assign array.
+ * Note: Ubah function menggunakan arrow function.
  */
 function update(index, user) {}
 
 /**
  * TODO 5.
- * Create destroy function: Delete data user.
- * Hint: use splice method.
- * Note: Use arrow function and const.
+ * Buat function destroy: Menghapus data user.
+ * Hint: Gunakan method splice.
+ * Note: Ubah function menggunakan arrow function.
  */
 function destroy(index) {}
 
 /**
- * Main Function.
- * Don't edit or remove this function.
- * This function for testing your task.
+ * Function main.
+ * Jangan edit atau hapus function main.
+ * Function ini untuk testing task.
  */
 
 const main = () => {
   /**
-   * Test index function
+   * Test function index
    */
   console.log("# Get All Users");
   all();
 
   /**
-   * Test store function
+   * Test function store
    */
   console.log("# Add New User: Sabiq");
   const newUser = {
@@ -69,7 +63,7 @@ const main = () => {
   store(newUser);
 
   /**
-   * Test update function
+   * Test function update
    */
   console.log("# Edit User: Isfa");
   const editedIndex = 1;
@@ -81,7 +75,7 @@ const main = () => {
   update(editedIndex, editedUser);
 
   /**
-   * Test destroy function
+   * Test function destroy
    */
   console.log("# Delete User: Nurul");
   const deletedIndex = 2;
@@ -89,3 +83,9 @@ const main = () => {
 };
 
 main();
+
+/**
+ * Jangan hapus exports.
+ * Exports ini untuk tujuan testing.
+ */
+module.exports = { users, all, store, update, destroy };
